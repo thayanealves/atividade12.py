3269656313e1cde35ac054941ad2268f042133d7
thayanealves <thayanemarciano4@gmail.com>
2024-09-11 14:48:46 -0300

    aprovado o reprovado

---
+++ atividade12.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+nota = int(input("digite a nota do aluno"))
+
+if  nota <=6 : print ("você está reprovado")
+elif  nota >=6<=10 : print ("você está aprovado")
